14e154ea96f9d98370edceeef13fe64a5b44d492
MLeongQA <mleong@qa.com>
2021-08-11 14:28:27 +0000

    not sure anymore

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-sudo chown -R mysql:mysql /var/lib/mysql/.
-
 docker-compose build --parallel && \
 #docker login -u ${DOCKERHUB_CREDENTIALS_USR} -p ${DOCKERHUB_CREDENTIALS_PSW}
 docker login -u mleongqa -p QACTrainee1
