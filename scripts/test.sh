1af867ec112aff5cfc4a860f8bcd34a00f33d453
MLeongQA <mleong@qa.com>
2021-08-12 12:05:22 +0000

    Updated script

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,5 +6,5 @@ python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
 
-python3 -m pytest --cov=. --cov-report=term-missing
+python3 -m pytest --cov --cov-report=term-missing
 
